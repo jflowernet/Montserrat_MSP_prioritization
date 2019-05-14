9477220d89703c9e99984a6c70af76b812d7e74b
Jason Flower <jflower@ucsb.edu>
2019-05-14 14:48:28 +0200

    added habitat rasters, prioritizr runs with varying habitat and species richness protection scenarios

--- scripts/marine_spatial_prioritization.R
+++ scripts/marine_spatial_prioritization.R
@@ -16,7 +16,7 @@ library(rgdal)
 library(sp)
 library(prioritizr)
 library(gurobi) 
-library(ipdw)
+library(tmap)
 
 ########################################################
 #Set projections
@@ -48,10 +48,10 @@ potscost <- rasterize(pots, plangrid, field = 1, update =TRUE)
 
 #import fishing value layer masked to 100m shelf and extent set to same as planning grid in ArcGIS
 fishingvalue <- raster("data/fishing_value")
-projection(fishing_new) <- mni_proj
+projection(fishingvalue) <- mni_proj
 
 #scale all values 0-1 by dividing by max value using log-transformed +1 fishing values (+1 so that zero values are transformed to zero not NA)
-fishing<- log(fishing_new+1)/ cellStats(log(fishing_new+1), max)
+fishing<- log(fishingvalue+1)/ cellStats(log(fishingvalue+1), max)
 
 #add fish pots layer to fishing value layer to get a cost layer
 cost <- potscost + fishing
@@ -63,10 +63,7 @@ cost <- reclassify(cost, c(-0.1, 0.1, 0.01))
 #Import habitat layers
 #################################################
 
-#rasterize habitat layer
-source("scripts/habitatRasters.R")
-
-filelist <- list.files("outputs/habitat_rasters/", ".tif", full.names = TRUE)
+filelist <- list.files("data/habitat_rasters/", ".tif", full.names = TRUE)
 habitatstack <- stack(filelist)
 
 #For the purposes of the prioritization, we combined all algal reefs (hard bottom and mixed bottom) to one habitat. Sand and Hard botom and sand combined to another single habitat category
@@ -85,9 +82,9 @@ source("scripts/interp_sprichness.R")
 
 species <- raster("outputs/speciesrich.tif")
 
-############################
+#############################################
 #Spatial prioritization using prioritizr
-#############################
+#############################################
 
 #create a stack with the habitat and species richness data
 features_stack<- stack(habitatstack_simplified, species)
@@ -103,3 +100,90 @@ prob <- problem(cost, features = features_stack) %>%
 sprob <- solve(prob)
 
 plot(sprob,  main = c("30% of 7 habitats, 50% species richness (interpolated), penalty = 0.001, edge = 0.5"))
+
+#################################################
+#10 - 40% area protection targets comparison
+#################################################
+
+#create the conservation problem - protect 10, 20, 30 or 40% of each habitat, 50% of total species richness, while minimizing cost (overlap with fishing effort proxy)
+prob_10perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.1, raster::nlayers(habitatstack_simplified)),0.5)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+prob_20perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.2, raster::nlayers(habitatstack_simplified)),0.5)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+prob_30perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.3, raster::nlayers(habitatstack_simplified)),0.5)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+prob_40perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.4, raster::nlayers(habitatstack_simplified)),0.5)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+#solve problem
+sprob_10perc <- solve(prob_10perc)
+sprob_20perc <- solve(prob_20perc)
+sprob_30perc <- solve(prob_30perc)
+sprob_40perc <- solve(prob_40perc)
+
+plotstack <- stack(sprob_10perc, sprob_20perc, sprob_30perc, sprob_40perc)
+
+#plot outputs
+tm_shape(plotstack)+
+tm_raster(palette = c("#c6c5c5", "#409a00"), n=2, legend.show = FALSE) +
+  tm_layout(title = c("(a) 10%", "(b) 20%", "(c) 30%", "(d) 40%")) 
+
+tmap_save(filename = "outputs/varying_habitat_targets.png")
+#################################################
+#Varying species richness feature target
+#################################################
+
+#create the conservation problem - protect 10, 30, 50 or 70% of total (summed) species richness, 30% of each habitat, while minimizing cost (overlap with fishing effort proxy)
+species_prob_10perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.3, raster::nlayers(habitatstack_simplified)), 0.1)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+species_prob_30perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.3, raster::nlayers(habitatstack_simplified)), 0.3)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+species_prob_50perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.3, raster::nlayers(habitatstack_simplified)), 0.5)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+species_prob_70perc <- problem(cost, features = features_stack) %>%
+  add_min_set_objective() %>%
+  add_relative_targets(c(rep(0.3, raster::nlayers(habitatstack_simplified)), 0.7)) %>%
+  add_binary_decisions() %>% 
+  add_boundary_penalties(penalty = .001, edge_factor = 0.5)
+
+#solve problem
+s_species_prob_10perc <- solve(species_prob_10perc)
+s_species_prob_30perc <- solve(species_prob_30perc)
+s_species_prob_50perc <- solve(species_prob_50perc)
+s_species_prob_70perc <- solve(species_prob_70perc)
+
+plotstack <- stack(s_species_prob_10perc, s_species_prob_30perc, s_species_prob_50perc, s_species_prob_70perc)
+
+#plot outputs
+tm_shape(plotstack)+
+  tm_raster(palette = c("#c6c5c5", "#409a00"), n=2, legend.show = FALSE) +
+  tm_layout(title = c("(a) 10%", "(b) 30%", "(c) 50%", "(d) 70%")) 
+
+tmap_save(filename = "outputs/varying_sprich_targets.png")
